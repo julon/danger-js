1e406af3f7c32c5e684ea29ff58c8a952844625e
Orta Therox <orta.therox@gmail.com>
2018-01-06 12:37:05 -0500

    Refactor the GitHub git DSL to the git DSL

--- source/commands/danger-local.ts
+++ source/commands/danger-local.ts
@@ -4,13 +4,26 @@ import * as program from "commander"
 
 import setSharedArgs, { SharedCLI } from "./utils/sharedDangerfileArgs"
 import { runRunner } from "./ci/runner"
-import { LocalGit } from "../platforms/local-git"
+import { LocalGit } from "../platforms/LocalGit"
 import { FakeCI } from "../ci_source/providers/Fake"
 
-program.usage("[options]").description("Runs danger without PR metadata, useful for git hooks.")
+interface App extends SharedCLI {
+  /** What should we compare against? */
+  base?: string
+  /** Should we run against current staged changes? */
+  staging?: boolean
+}
+
+program
+  .usage("[options]")
+  .option("-b, --base  [base]", "Choose the base commit to work against.")
+  // TODO: this option
+  // .option("-s, --staging", "Just use staged changes.")
+  .description("Runs danger without PR metadata, useful for git hooks.")
 setSharedArgs(program).parse(process.argv)
 
-const app = (program as any) as SharedCLI
-const localPlatform = new LocalGit()
+const app = (program as any) as App
+const base = app.base || "master"
+const localPlatform = new LocalGit({ base, staged: app.staging })
 const fakeSource = new FakeCI(process.env)
 runRunner(app, { source: fakeSource, platform: localPlatform })
